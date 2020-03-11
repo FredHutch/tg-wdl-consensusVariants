d0eaf7ffcdf2fbe4c09d414830bd0b421f3e89d0
Amy Paguirigan <amypaguirigan@gmail.com>
2020-03-10 17:44:42 -0700

    now with Trio

--- unpaired/consensus-trio.R
+++ unpaired/consensus-trio.R
@@ -60,50 +60,45 @@ S <- S %>% mutate_if(is.integer, as.character);
 
 print("Process Mutect variants")
 
-Mu <- read.delim(file = SAMfile, row.names=NULL, 
+Mu <- read.delim(file = MutectFile, row.names=NULL, 
                 header = FALSE, skip = 1)
-annHead <- read.delim(file = SAMfile, row.names = NULL, 
+annHead <- read.delim(file = MutectFile, row.names = NULL, 
                       nrows = 1, header=FALSE, stringsAsFactors = FALSE)
-extraHead <- c("QUAL1", "DP.SAM", "CHR", "POS", "END", "REF", 
-               "ALT", "QUAL.SAM", "FILTER", "INFO.SAM", "FORMAT.SAM", "MAGIC.SAM")
+extraHead <- c("col1", "DP.Mu", "CHR", "POS", "END", "REF", 
+               "ALT", "col2", "FILTER.Mu", "INFO.Mu", "FORMAT.Mu", "MAGIC.Mu")
 colnames(Mu) <- c(annHead[1,], extraHead)
-#DP4 = Number of 1) forward ref alleles; 2) reverse ref; 3) forward non-ref; 4) reverse non-ref alleles,
-# used in variant calling. Sum can be smaller than DP because low-quality bases are not counted.
-Mu[,"QUAL1"]<- NULL
+Mu[,"col1"]<- NULL
 Mu <- Mu %>% filter(Otherinfo %in% c("0", "0.5", "1"))
 Mu$VariantID <- paste(Mu$CHR, Mu$Gene.refGene, Mu$POS, Mu$REF, Mu$ALT, sep = "-")
-a <- gsub(".*:", "",Mu$MAGIC.SAM);
-REF <- as.numeric(gsub(",.*$","",a));
-Mu$AD.SAM <- as.numeric(gsub("^.*,","",a));
-Mu$DP.SAM <- REF+Mu$AD.SAM;
-Mu$VAF.SAM <- 100*Mu$AD.SAM/Mu$DP.SAM;
+Mu$DP.Mu <- as.integer(Mu$DP.Mu)
+Mu$VariantID <- paste(Mu$CHR, Mu$Gene.refGene, Mu$POS, Mu$REF, Mu$ALT, sep = "-")
+Mu$AD.Mu <- as.integer(gsub("^[^:]+:[^,]+,|:.*", "", Mu$MAGIC.Mu));
+Mu$VAF.Mu <- 100*as.numeric(Mu$AD.Mu)/as.numeric(Mu$DP.Mu);
 Mu <- Filter(function(y)!all(y == "."), Mu);
 Mu[Mu == ""] <- NA
-Mu <- Mu %>% mutate_if(is.factor, as.character);
+Mu <- Mu %>% dplyr::mutate_if(is.factor, as.character);
 Mu <- Mu %>% mutate_if(is.integer, as.character);
-
-
-
-
 
 ## Deal with flubbed annotation issues where the same variant is now annotated two different ways!  ARGH!
 commonCols <- c("VariantID","CHR", "POS", "REF", "ALT", "Chr", "Start", "End", "Ref", "Alt")
-GATKCols <- colnames(G)[!colnames(G) %in% colnames(S)]
-SAMCols <- colnames(S)[!colnames(S) %in% colnames(G)]
+GATKCols <- colnames(G)[!colnames(G) %in% c(colnames(S), colnames(Mu))]
+SAMCols <- colnames(S)[!colnames(S) %in% c(colnames(G), colnames(Mu))]
+MuCols <- colnames(Mu)[!colnames(Mu) %in% c(colnames(G), colnames(S))]
 
-GMerge <- G %>% select(c(commonCols, GATKCols))
-SMerge <- S %>% select(c(commonCols, SAMCols))
+GMerge <- G %>% select(all_of(c(commonCols, GATKCols)))
+SMerge <- S %>% select(all_of(c(commonCols, SAMCols)))
+MuMerge <- Mu %>% select(all_of(c(commonCols, MuCols)))
 
-variants <- full_join(GMerge, SMerge)
+variants <- full_join(full_join(GMerge, SMerge), MuMerge)
 variants$Type <- ifelse(nchar(variants$REF) == nchar(variants$ALT), "SNV", "INDEL");
 
 reannotate <- left_join(variants, G)
 reannotate <- left_join(reannotate, S)
+reannotate <- left_join(reannotate, Mu)
 
-reannotate$VAF.DIFF <- reannotate$VAF.GATK-reannotate$VAF.SAM;
-reannotate$Confidence  <- ifelse(!is.na(reannotate$AD.GATK) & !is.na(reannotate$AD.SAM), "conftier1",
-                  ifelse(is.na(reannotate$AD.GATK) & !is.na(reannotate$AD.SAM) & reannotate$Type == "SNV", "conftier2",
-                         ifelse(!is.na(reannotate$AD.GATK) & is.na(reannotate$AD.SAM) & reannotate$Type == "INDEL", "conftier2", 
+reannotate$Confidence  <- ifelse(!is.na(reannotate$AD.GATK) & !is.na(reannotate$AD.SAM) & !is.na(reannotate$AD.Mu), "conftier1",
+                  ifelse((is.na(reannotate$AD.GATK) | is.na(reannotate$AD.Mu)) & !is.na(reannotate$AD.SAM) & reannotate$Type == "SNV", "conftier2",
+                         ifelse(!(is.na(reannotate$AD.GATK) | is.na(reannotate$AD.Mu)) & is.na(reannotate$AD.SAM) & reannotate$Type == "INDEL", "conftier2", 
                                 "conftier3")))
 write.table(reannotate, file = paste0(baseName, ".consensus.tsv"), sep = "\t",
             row.names = F)
